2f8ed219f146bf3bbd81ef447bf7ca3cd48ca0c8
DiaVanessaHC18 <d.vane.huancacruz@gmail.com>
2024-09-08 12:20:40 -0500

    Add files via upload

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/control/CalcControl.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/control/CalcControl.java
@@ -1,5 +1,6 @@
 package pe.edu.upeu.calcfx.control;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,60 +9,52 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CalcControl {
- @FXML
+
+    @FXML
     TextField txtResultado;
 
- @FXML
+
+
+    @FXML
     public void accionButton(ActionEvent event){
-     System.out.println("Holas");
-     Button button =(Button) event.getSource();
-     switch (button.getId()){
-         case "btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9","btn0":{
-             EscribirNumeros(button.getText());
-         }break;
-         case "btnsuma","btnresta","btnMult","btnDiv":{
-             operador(button.getText());
-         }break;
-         case "txtResulatdo":{
-             calcularResultado();
-         }break;
-         case "btnborrar":{
-             txtResultado.clear();
-         }
-     }
+        System.out.println("Holas");
+        Button button= (Button)event.getSource();
+        switch (button.getId()){
+            case "btn7","btn8","btn9","btn6","btn5","btn4","btn3","btn2","btn1","btn0":{
+                escribirNumeros(button.getText());
+            }break;
+            case "btnsuma", "btnmulti", "btnresta", "btndivi":{
+                operador(button.getText());
+            }break;
+            case "btnIgual":{
+                calcularResultado();
+            }break;
+            case "btnBorrar":{
+                txtResultado.clear();
+            }
 
- }
-public void EscribirNumeros(String valor){
-     txtResultado.appendText(valor);
-}
+        }
 
+    }
 
-public void operador(String valor){
-     txtResultado.appendText(""+valor+"");
-}
+    public void escribirNumeros(String valor){
+        txtResultado.appendText(valor);
+    }
 
-public void calcularResultado(){
-     String[] valores=txtResultado.getText().split("");
-     double val1=Double.parseDouble(String.valueOf(valores[0]));
-     double val2=Double.parseDouble(String.valueOf(valores[2]));
-     switch (valores[1]){
-         case "+":{txtResultado.setText(String.valueOf(val1+val2));}break;
-         case "-":{txtResultado.setText(String.valueOf(val1-val2));}break;
-         case "*":{txtResultado.setText(String.valueOf(val1*val2));}break;
-         case "/":{txtResultado.setText(String.valueOf(val1/val2));}break;
+    public void operador(String valor){
+        txtResultado.appendText(" "+valor+" ");
+    }
 
-     }
-    System.out.println(valores[0]);
-    System.out.println(valores[1]);
-    System.out.println(valores[2]);
-}
-
-
-
+    public  void calcularResultado(){
+        String[] valores=txtResultado.getText().split(" ");
+        double val1=Double.parseDouble(String.valueOf(valores[0]));
+        double val2=Double.parseDouble(String.valueOf(valores[2]));
+        switch (valores[1]){
+            case "+":{txtResultado.setText(String.valueOf(val1+val2));}break;
+            case "-":{txtResultado.setText(String.valueOf(val1-val2));}break;
+            case "/":{txtResultado.setText(String.valueOf(val1/val2));}break;
+            case "*":{txtResultado.setText(String.valueOf(val1*val2));}break;
+        }
+    }
 
 }
-
-
-
-
-
